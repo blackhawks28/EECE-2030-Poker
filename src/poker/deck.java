ba9b0e2355906fa5cde981764f4ab3b5281c6fe5
drburmeister <60713533+drburmeister@users.noreply.github.com>
2020-02-09 14:01:41 -0600

    Initial commit

---
+++ src/poker/deck.java
@@ -0,0 +1,134 @@
+package poker;
+
+public class deck {
+	private static int Ace;
+	private static int King;
+	private static int Queen;
+	private static int Jack;
+	private static int ten;
+	private static int nine;
+	private static int eight;
+	private static int seven;
+	private static int six;
+	private static int five;
+	private static int four;
+	private static int three;
+	private static int two;
+	
+	public static void incrAce() {
+		Ace++;
+	}
+	public static void incrKing() {
+		King++;
+	}
+	public static void incrQueen() {
+		Queen++;
+	}
+	public static void inccJack() {
+		Jack++;
+	}
+	public static void incrTen() {
+		ten++;
+	}
+	public static void incrNine() {
+		nine++;
+	}
+	public static void incrEight() {
+		eight++;
+	}
+	public static void incrSeven() {
+		seven++;
+	}public static void incrSix() {
+		six++;
+	}
+	public static void incrFive() {
+		five++;
+	}
+	public static void incrFour() {
+		four++;
+	}
+	public static void incrThree() {
+		three++;
+	}
+	public static void incrTwo() {
+		two++;
+	}
+	public static int getAce() {
+		return Ace;
+	}
+	public static void setAce(int ace) {
+		Ace = ace;
+	}
+	public static int getKing() {
+		return King;
+	}
+	public static void setKing(int king) {
+		King = king;
+	}
+	public static int getQueen() {
+		return Queen;
+	}
+	public static void setQueen(int queen) {
+		Queen = queen;
+	}
+	public static int getJack() {
+		return Jack;
+	}
+	public static void setJack(int jack) {
+		Jack = jack;
+	}
+	public static int getTen() {
+		return ten;
+	}
+	public static void setTen(int ten) {
+		deck.ten = ten;
+	}
+	public static int getNine() {
+		return nine;
+	}
+	public static void setNine(int nine) {
+		deck.nine = nine;
+	}
+	public static int getEight() {
+		return eight;
+	}
+	public static void setEight(int eight) {
+		deck.eight = eight;
+	}
+	public static int getSeven() {
+		return seven;
+	}
+	public static void setSeven(int seven) {
+		deck.seven = seven;
+	}
+	public static int getSix() {
+		return six;
+	}
+	public static void setSix(int six) {
+		deck.six = six;
+	}
+	public static int getFive() {
+		return five;
+	}
+	public static void setFive(int five) {
+		deck.five = five;
+	}
+	public static int getFour() {
+		return four;
+	}
+	public static void setFour(int four) {
+		deck.four = four;
+	}
+	public static int getThree() {
+		return three;
+	}
+	public static void setThree(int three) {
+		deck.three = three;
+	}
+	public static int getTwo() {
+		return two;
+	}
+	public static void setTwo(int two) {
+		deck.two = two;
+	}
+}
