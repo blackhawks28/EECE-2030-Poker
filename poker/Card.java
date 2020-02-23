6fdbaad3c519bfb9f61f7b95aaba6df49086c9e4
drburmeister <60713533+drburmeister@users.noreply.github.com>
2020-02-23 15:37:46 -0600

    Add files via upload

--- poker/Card.java
+++ poker/Card.java
@@ -6,13 +6,134 @@ public class Card {
 	int suit;
 	
 	public Card(int number, int suit) {
+
 		this.number = number;
 		this.suit = suit;
 		
 	}
+	
+	
+	public static int handVals(Card[] Hand) {
+		int sum = 0;
+		for (int i=0;i<5;i++) {
+			if (Hand[i].getNum() == 1) {
+				sum+= 14; 
+			}
+			else 
+				sum += Hand[i].getNum();
+		}
+		return sum;
+	}
+	
 	public int getNum() {
 		return number;
 	}
+	
+	public int getSuit() {
+		return suit;
+	}
+	public void setNum(int newNum) {
+		this.number=newNum;
+	}
+	
+	public static int pair(Card[] Hand) {	
+		
+		int parry = 0;
+		int hold = 0;
+		int hold2 = 0;
+		int kind = 1;
+		int kind2 = 1;
+		
+		for (int i = 0; i < 4; i++) {
+			
+			if (hold != 0 && kind < 2) {
+				kind = 2;
+				parry = 1;
+				}
+			if (hold2 != 0 && kind2 < 2)
+				kind2 = 2;
+			
+			for (int j = 1 + i; j<5; j++) {
+				
+				if (j == i) {
+					j++;
+				}
+//				System.out.println(Hand[i].getNum() + " " +  Hand[j].getNum() + " " + kind + " " + kind2); 
+					if (Hand[i].getNum() == Hand[j].getNum() && Hand[i].getNum() == hold && Hand[i].getNum() != 0) {
+						kind++;
+				}
+					if (Hand[i].getNum() == Hand[j].getNum() && Hand[i].getNum() == hold2  && Hand[i].getNum() != 0) {
+						kind2++;
+				}
+					if (Hand[i].getNum() == Hand[j].getNum() && Hand[i].getNum() != hold && hold2 == 0 && hold != 0  && Hand[i].getNum() != 0) {
+						parry++;
+						hold2 = Hand[i].getNum();
+				}
+					if (Hand[i].getNum() == Hand[j].getNum() && hold == 0  && Hand[i].getNum() != 0) {
+						hold = Hand[i].getNum();
+				}
+			}
+		}
+//		System.out.println(kind + " " + kind2);
+		if (kind >= 4 || kind2 >= 4)
+			return 5000;
+		if ((kind == 2 && kind2 == 3) || (kind == 3 && kind2 == 2))
+			return 4000;
+		if (kind == 3)
+			return 2000;
+		if (parry == 2)
+			return 500;
+		if (kind == 2)
+			return 250;
+		else return 0;
+	}
+	
+	public static int flush(Card[] Hand) {
+				if (Hand[0].getSuit() == Hand[1].getSuit() && Hand[0].getSuit() == Hand[2].getSuit() 
+				&& Hand[0].getSuit() == Hand[3].getSuit() && Hand[0].getSuit() == Hand[4].getSuit() && Hand[0].getSuit() != 0) {
+			return 3000;
+		}
+			return 0;
+	}
+	
+	public static void sort(Card[] Hand) {
+
+		int hold;
+		for (int i = 0; i < Hand.length; i++) {
+			for (int j = i + 1; j < Hand.length; j++) {
+				if ((Hand[i].getNum() > Hand[j].getNum()) && i != j) {
+					hold = Hand[i].getNum();
+					Hand[i].setNum(Hand[j].getNum());
+					Hand[j].setNum(hold);
+				}
+			}
+		}
+		for (int i = 0; i < Hand.length; i++) {
+			System.out.println(Hand[i]);
+		}
+	}
+
+	public static int straight(Card [] Hand){
+		int count = 1;
+		int t = 0;
+
+		for(int i = Hand.length-1; i >= 1; i--){
+			for(int j = i-1; j >= 0; j--){
+				if((Hand[i].getNum() == 0) || (Hand[j].getNum() == 0)){
+					continue;
+				}
+				 if((Hand[i].getNum() - 1) == (Hand[j].getNum())) {
+					  count++;
+				 }
+					}
+				}
+		if (count == 5) {
+//			System.out.println("You have a straight.");
+			return 2500;
+		}
+		return 0;
+	}
+ 
 	
 	public String toString(){
 		String s = "Spades";
--- poker/handValue.java
+++ poker/handValue.java
@@ -1,17 +1,28 @@
 package poker;
 
 public class handValue {
-	public static Card[] Hand = new Card[7];
-	
-	public int pair(Card[] Hand) {	
-		for (int i = 0; i<7; i++) {
-			for (int j = 1; j<7; j++) {
-				if (j == i) j++;
-					if (Hand[i].getNum() == Hand[j].getNum) {
-					
-				}
+	public static int bestHand(Card[] Hand) {
+		Card[] best = new Card[5];
+		Card[] empty = new Card [5];
+		Card[] hold = empty;
+		
+		for(int i=0; i<3;i++) {	
+			if (hold != null && best != null) {
+				if (pokerDriver.test(hold) > pokerDriver.test(best))
+					best = hold;
+			}
+			int index = 1;
+			hold = empty;
+			hold[0] = Hand[i];
+			
+			for(int j=i+1; j<i+5;j++) {
+				hold[index] = Hand[j];
+				System.out.println(hold[0].getNum()+ " " + hold[index].getNum());
+				index++;
 			}
 		}
-		return 10;
+		
+		return pokerDriver.test(best);
 	}
+
 }
