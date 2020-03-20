16c1a5039e1c857dd7b4d18cd0943b37c9e78efd
drburmeister <60713533+drburmeister@users.noreply.github.com>
2020-03-19 21:51:46 -0500

    Sprint 2

--- poker/Card.java
+++ poker/Card.java
@@ -2,6 +2,7 @@ package poker;
 
 public class Card {
 	
+	//Setting up card class that will be used throughout
 	int number;
 	int suit;
 	
@@ -11,31 +12,53 @@ public class Card {
 		this.suit = suit;
 		
 	}
+	//Setting a null card if necessary
+	public static Card setZero() {
+		Card zero = new Card(0,0);
+		return zero;
+	}
 	
-	
+	//Method to return numerical hand values for tiebreakers
 	public static int handVals(Card[] Hand) {
+		Card high = new Card(0,0);
+		int ace = 14;
 		int sum = 0;
+		
+		//Cycling through cards
 		for (int i=0;i<5;i++) {
+			if ((Hand[i].getNum() > high.getNum() || (Hand[i].getNum() == 1)) && (high.getNum() != 1)) {
+				high.setNum(Hand[i].getNum());
+				high.setSuit(Hand[i].getSuit());
+			}
+			
+			//Ave has the highest numerical value but is defaulted to 1
 			if (Hand[i].getNum() == 1) {
-				sum+= 14; 
+				sum+= ace; 
 			}
 			else 
 				sum += Hand[i].getNum();
 		}
+	//	System.out.println("Your highest card is a(n) " + high);
 		return sum;
 	}
-	
+	//Getter of number value for Card objects
 	public int getNum() {
 		return number;
 	}
-	
+	//Getter of suit number for Card objects
 	public int getSuit() {
 		return suit;
 	}
+	//Setter of number value for Card objects
 	public void setNum(int newNum) {
 		this.number=newNum;
 	}
+	//Setter of suit number for Card objects
+	public void setSuit(int newSuit) {
+		this.suit = newSuit;
+	}
 	
+	//Checks for pair, 2pair, 3 of a Kind, full house, and four of a kind
 	public static int pair(Card[] Hand) {	
 		
 		int parry = 0;
@@ -44,21 +67,28 @@ public class Card {
 		int kind = 1;
 		int kind2 = 1;
 		
+		//Nested for loops for gathering every combination of cards
 		for (int i = 0; i < 4; i++) {
 			
+			//Checking if pair ever formed on second loop through
 			if (hold != 0 && kind < 2) {
 				kind = 2;
 				parry = 1;
 				}
+			
+			//Checking if a second pair ever formed
 			if (hold2 != 0 && kind2 < 2)
 				kind2 = 2;
 			
-			for (int j = 1 + i; j<5; j++) {
+			for (int j = 1 + i; j < 5; j++) {
 				
+				//Card can't check itself
 				if (j == i) {
 					j++;
 				}
 //				System.out.println(Hand[i].getNum() + " " +  Hand[j].getNum() + " " + kind + " " + kind2); 
+				
+				//Logic for checking for pairs
 					if (Hand[i].getNum() == Hand[j].getNum() && Hand[i].getNum() == hold && Hand[i].getNum() != 0) {
 						kind++;
 				}
@@ -71,36 +101,50 @@ public class Card {
 				}
 					if (Hand[i].getNum() == Hand[j].getNum() && hold == 0  && Hand[i].getNum() != 0) {
 						hold = Hand[i].getNum();
+//						kind++;
+//						parry++;
 				}
 			}
 		}
 //		System.out.println(kind + " " + kind2);
-		if (kind >= 4 || kind2 >= 4)
+		
+		//Return value cases for each type of check
+		if (kind >= 4 || kind2 >= 4) {
 			return 5000;
-		if ((kind == 2 && kind2 == 3) || (kind == 3 && kind2 == 2))
+		}
+		if ((kind == 2 && kind2 == 3) || (kind == 3 && kind2 == 2)) {
+		
 			return 4000;
-		if (kind == 3)
+		}
+		if (kind == 3) {
 			return 2000;
-		if (parry == 2)
+		}
+		if (parry == 2) {
 			return 500;
-		if (kind == 2)
+		}
+		if (kind == 2) {
 			return 250;
+		}
 		else return 0;
 	}
 	
+	//Method for checking for a flush with O(1) efficiency
 	public static int flush(Card[] Hand) {
 				if (Hand[0].getSuit() == Hand[1].getSuit() && Hand[0].getSuit() == Hand[2].getSuit() 
 				&& Hand[0].getSuit() == Hand[3].getSuit() && Hand[0].getSuit() == Hand[4].getSuit() && Hand[0].getSuit() != 0) {
-			return 3000;
+				
+					return 3000;
 		}
 			return 0;
 	}
 	
+	//Sorting method for straight check
 	public static void sort(Card[] Hand) {
 
 		int hold;
-		for (int i = 0; i < Hand.length; i++) {
-			for (int j = i + 1; j < Hand.length; j++) {
+		//Formats Cards into the array least to greatest
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 4; j++) {
 				if ((Hand[i].getNum() > Hand[j].getNum()) && i != j) {
 					hold = Hand[i].getNum();
 					Hand[i].setNum(Hand[j].getNum());
@@ -108,16 +152,16 @@ public class Card {
 				}
 			}
 		}
-		for (int i = 0; i < Hand.length; i++) {
-			System.out.println(Hand[i]);
-		}
 	}
 
+	//Checking for straight method
 	public static int straight(Card [] Hand){
+
 		int count = 1;
 		int t = 0;
-
-		for(int i = Hand.length-1; i >= 1; i--){
+		
+		//Loops that run through the Card array and increments if conditions are met
+		for(int i = 4; i >= 1; i--){
 			for(int j = i-1; j >= 0; j--){
 				if((Hand[i].getNum() == 0) || (Hand[j].getNum() == 0)){
 					continue;
@@ -127,6 +171,8 @@ public class Card {
 				 }
 					}
 				}
+		
+		//Returning value if straight is true
 		if (count == 5) {
 //			System.out.println("You have a straight.");
 			return 2500;
@@ -134,7 +180,7 @@ public class Card {
 		return 0;
 	}
  
-	
+	//Method for printing the Card object
 	public String toString(){
 		String s = "Spades";
 		if (suit == 0)
@@ -159,5 +205,19 @@ public class Card {
 			return "Jack of " + s;
 		else
 			return number + " of " + s; 
+	}
+
+	public static String printHand(Card[] Hand){
+		String s = "";
+		for(int i=0; i<Hand.length; i++){
+			
+			if(Hand[i] != null) {
+				s+= Hand[i];
+				if(i != Hand.length && Hand[i+1] == null)
+					break;
+				s+= ", ";
+			}
+		}
+		return s;
 	}
 }
--- poker/pokerDriver.java
+++ poker/pokerDriver.java
@@ -1,24 +1,191 @@
 package poker;
 
+import java.util.Scanner;
 
+//Main Method Class
 public class pokerDriver {
 
+	static Scanner scnr = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Card[] test = new Card[7];
-		test[0] = new Card(2,2);
-		test[1] = new Card(2,2);
-		test[2] = new Card(5,2);
-		test[3] = new Card(12,1);
-		test[4] = new Card(1,3);
-		System.out.println(Card.pair(test));
-		System.out.println(handValue.bestHand(test));
+		
+		//Driver Program to test objects and methods created in other classes
+		System.out.println("Welcome to the Poker Hand Reader! \nTo use this program follow the prompts showm. "
+				+ "\nCards are inputted as follows, \"kh\" for King of Hearts, \"10s\" for 10 of spades, \"ad\" For Ace of Diamonds,"
+				+ " and so on. \nType \"fold\" at any time to restart the hand or \"Close\" to stop the program. Enjoy!");
+		poker();
 	}
+		
+	public static void poker() {
+		
+			System.out.println();
+			Card[] Hand = new Card[7];
+		
+			Hand = pre();
+			System.out.println("The cards inputted are:\n" + Card.printHand(Hand));
+			
+			Card[] Flop = new Card[3];
+			Flop = flop();
+			Hand[2] = Flop[0]; Hand[3] = Flop[1]; Hand[4] = Flop[2];
+			System.out.println("The cards inputted are:\n" + Card.printHand(Hand));
+			System.out.println("You have a hand value of: " + handValue.bestHand(Hand));
+			
+			Hand[5] = turn();
+			System.out.println("You have a hand value of: " + handValue.bestHand(Hand));
+			
+			Hand[6] = river();
+			System.out.println("You have a hand value of: " + handValue.bestHand(Hand));
 
+			System.out.println();
+			poker();
+	}
+	
+	public static int inputCheck(String input) {
+		
+		int n = 0;
+		
+		if (input.compareTo("a") == 0)
+			n = 1;
+		if (input.compareTo("2") == 0)
+			n = 2;
+		if (input.compareTo("3") == 0)
+			n = 3;
+		if (input.compareTo("4") == 0)
+			n = 4;
+		if (input.compareTo("5") == 0)
+			n = 5;
+		if (input.compareTo("6") == 0)
+			n = 6;
+		if (input.compareTo("7") == 0)
+			n = 7;
+		if (input.compareTo("8") == 0)
+			n = 8;
+		if (input.compareTo("9") == 0)
+			n = 9;
+		if (input.compareTo("10") == 0)
+			n = 10;
+		if (input.compareTo("j") == 0)
+			n = 11;
+		if (input.compareTo("q") == 0)
+			n = 12;
+		if (input.compareTo("k") == 0)
+			n = 13;
+		
 
-	public static int test(Card[] Hand) {
-	if (Hand != null)
-		return Card.pair(Hand) + Card.flush(Hand) + Card.straight(Hand) + Card.handVals(Hand);
-	else
-		return 0;
+		if(input.equals("fold")) {
+			System.out.println("You've decided to fold.");
+			poker();
+		}	
+		else if (input.equals("close") ||input.equals("Close")) {
+			System.out.println("You've decided to close the program.");
+			System.exit(0);
+		}		
+		else if (n ==0) {
+			System.out.println("Invalid Card Entered... Restarting");
+			poker();
+		}
+			
+		return n;
+	}		
+	public static int inputCheck2(String input2) {
+		int s = 0;
+		if (input2.compareTo("c") == 0)
+			s = 1;
+		if (input2.compareTo("d") == 0)
+			s = 2;
+		if (input2.compareTo("h") == 0)
+			s = 3;
+		if (input2.compareTo("s") == 0)
+			s = 4;
+		
+		if(input2.equals("fold")) {
+			System.out.println("You've decided to fold.");
+			poker();
+		}			
+		else if(input2.equals("close") ||input2.equals("Close")) {
+			System.out.println("You've decided to close the program.");
+			System.exit(0);
+		}		
+		else if (s == 0) {
+			System.out.println("Invalid Suit Entered... Restarting");
+			poker();
+		}
+		return s;
+	}
+	public static Card cardMaker(int n, int s) {
+		Card thing = new Card(n,s);
+		return thing;
+	}
+	
+	public static Card[] pre() {
+		
+		Card[] pre = new Card[7];
+		String input = "";
+		String input2 = "";
+		System.out.println("Enter your dealt hand cards. First the Card number/letter, enter, and then the suit for each card.");
+		
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		pre[0] = cardMaker(inputCheck(input), inputCheck2(input2));
+		
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		pre[1] = cardMaker(inputCheck(input), inputCheck2(input2));
+		
+		
+		return pre;
+	}
+	private static Card[] flop() {
+		
+		Card[] flop = new Card[3];
+		String input = "";
+		String input2 = "";
+		
+		System.out.println("Enter the three flop cards.");
+		
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		flop[0] = cardMaker(inputCheck(input), inputCheck2(input2));
+
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		flop[1] = cardMaker(inputCheck(input), inputCheck2(input2));
+
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		flop[2] = cardMaker(inputCheck(input), inputCheck2(input2));
+		
+		return flop;
+	}
+	private static Card turn() {
+		
+		Card turn = new Card(0,0);
+		String input = "";
+		String input2 = "";
+		
+		System.out.println("Enter the turn card.");
+		
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		turn = cardMaker(inputCheck(input), inputCheck2(input2));
+		
+		return turn;
+			
+	}
+	private static Card river() {
+		
+		Card river = new Card(0,0);
+		String input = "";
+		String input2 = "";
+		
+		System.out.println("Enter the river card.");
+		
+		input = scnr.nextLine();
+		input2 = scnr.nextLine();
+		river = cardMaker(inputCheck(input), inputCheck2(input2));
+		
+		return river;
+			
 	}
 }
+		
